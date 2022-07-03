72fea979f8359bc57f0c445567b18d16cb58d95f
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-07-03 11:32:12 -0500

    search render working

--- src/components/Browse/Browse.jsx
+++ src/components/Browse/Browse.jsx
@@ -2,14 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-
 // mui imports
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
 import Box from '@mui/material/Box';
+import CardContent from '@mui/material/CardContent';
+//mui toggle
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+// mui icons 
+import ListIcon from '@mui/icons-material/List';
+import GridViewIcon from '@mui/icons-material/GridView';
 
 function Browse(){
+    const results = useSelector(store => store.browseBasic.results);
     const [search, setSearch]= useState('');
     const dispatch = useDispatch();
     const history = useHistory();
@@ -21,7 +31,18 @@ function Browse(){
             payload: search
         });
         setSearch('');
-        history.push(`/results`);
+    }
+
+    // to toggle the view
+    const [display, setDisplay] = useState(true);
+    const toggleDisplay = () => {
+        setDisplay(!display);
+        console.log(display)
+    }
+ 
+    // push to detailed page on cover click
+    function detailedView(){
+        history.push(`/details`);
     }
 
     return (
@@ -51,6 +72,59 @@ function Browse(){
                     />
                     <Button onClick={sendSearch} variant="contained">search</Button>
                 </Box>
+
+                <Box
+                    m={1}
+                    //margin
+                    display="flex"
+                    justifyContent="flex-end"
+                    alignItems="flex-end"
+                >
+                    <ToggleButtonGroup >
+                        <ToggleButton value={!display} onClick={toggleDisplay}>
+                            <ListIcon />
+                        </ToggleButton>
+                    
+                        <ToggleButton value={display} onClick={toggleDisplay}>
+                            <GridViewIcon />
+                        </ToggleButton>
+                    
+                    </ToggleButtonGroup>
+                </Box>
+    
+                { results ?
+                <section className="flex-container-grid">
+                {results.map(record => {
+                    return (
+                        <div className='cards'>
+                        <Card sx={{ 
+                            maxWidth: 200, 
+                            minWidth: 200,  
+                            maxHeight: 200, 
+                            minHeight: 200 
+                        }}>
+                            <CardActionArea>
+                            <CardMedia
+                                onClick={detailedView}
+                                component="img"
+                                image= {record.cover_image}
+                                alt= {record.title}
+                                sx={{ 
+                                    maxWidth: 200, 
+                                    minWidth: 200,  
+                                    maxHeight: 200, 
+                                    minHeight:200 
+                                }}
+                            /> 
+                            </CardActionArea>
+                        </Card>
+                        </div>
+                    ) 
+                })}
+                </section>
+                :
+                <p>searh something loser</p>
+            }
             </div> 
         </div>
     )
--- src/components/BrowseResults/BrowseResults.jsx
+++ src/components/BrowseResults/BrowseResults.jsx
@@ -35,7 +35,13 @@ function BrowseResults(){
     }
 
     return(
-        <>
+        <>results! yay!</>
+    )
+}
+
+export default BrowseResults; 
+
+{/* <>
         <Box
             m={1}
             //margin
@@ -94,97 +100,6 @@ function BrowseResults(){
                             component="img"
                             sx={{ width: 150 }}
                             image={record.cover_image}
-                            alt={record.title}
-                        />
-                        <CardActionArea onClick={detailedView}>
-                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                            <CardContent sx={{ flex: '1 0 auto' }}>
-                            <Typography component="div" variant="h5">
-                                {record.title}
-                            </Typography>
-                            <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {record.year}
-                            </Typography>
-                            <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {record.genre}
-                            </Typography>
-                            <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {record.style}
-                            </Typography>
-                            </CardContent>
-                        </Box>
-                        </CardActionArea >
-                        </Card>
-                    </div>
-                ) 
-            })}
-            </section>  
-        }
-        </>
-    )
-}
-
-export default BrowseResults; 
-
-{/* <>
-        <Box
-            m={1}
-            //margin
-            display="flex"
-            justifyContent="flex-end"
-            alignItems="flex-end"
-        >
-            <ToggleButtonGroup >
-                <ToggleButton value={!display} onClick={toggleDisplay}>
-                    <ListIcon />
-                </ToggleButton>
-            
-                <ToggleButton value={display} onClick={toggleDisplay}>
-                    <GridViewIcon />
-                </ToggleButton>
-            
-            </ToggleButtonGroup>
-        </Box>
-            { display ?
-                <section className="flex-container-grid">
-                {results.map(record => {
-                    return (
-                        <div className='cards'>
-                        <Card sx={{ 
-                            maxWidth: 200, 
-                            minWidth: 200,  
-                            maxHeight: 200, 
-                            minHeight: 200 
-                        }}>
-                            <CardActionArea>
-                            <CardMedia
-                                onClick={detailedView}
-                                component="img"
-                                image= {record.cover}
-                                alt= {record.title}
-                                sx={{ 
-                                    maxWidth: 200, 
-                                    minWidth: 200,  
-                                    maxHeight: 200, 
-                                    minHeight:200 
-                                }}
-                            /> 
-                            </CardActionArea>
-                        </Card>
-                        </div>
-                    ) 
-                })}
-                </section>
-            :
-            <section className="flex-container-list">
-            {results.map(record => {
-                return (
-                    <div className='cards'>
-                        <Card sx={{ display: 'flex' }}>
-                        <CardMedia
-                            component="img"
-                            sx={{ width: 150 }}
-                            image={record.cover}
                             alt={record.title}
                         />
                         <CardActionArea onClick={detailedView}>
