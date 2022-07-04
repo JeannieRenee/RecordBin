8934c6a318940f51e6c091187b2b40f415af4df1
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-07-04 18:13:30 -0500

    put route works

--- server/routes/records.router.js
+++ server/routes/records.router.js
@@ -39,68 +39,68 @@ router.put('/:id', (req, res) => {
 })
 
 // delete record from db 
-router.delete('/:id', (req, res) => {
-  let recordId = req.params.id;
-  console.log('Delete request for id', recordId);
-  let sqlQuery = `
-    DELETE FROM "records" 
-    WHERE "id" = $1;
-  `;
-  const sqlParams = [
-    recordId,             
-  ];
-  pool.query(sqlQuery, sqlParams)
-    .then(() => {
-      console.log('task deleted');
-      res.sendStatus(204);
-    }).catch( (error) => {
-      console.log(`Error making database query`, error);
-      res.sendStatus(500); 
-    })
-})
+// router.delete('/:id', (req, res) => {
+//   let recordId = req.params.id;
+//   console.log('Delete request for id', recordId);
+//   let sqlQuery = `
+//     DELETE FROM "records" 
+//     WHERE "id" = $1;
+//   `;
+//   const sqlParams = [
+//     recordId,             
+//   ];
+//   pool.query(sqlQuery, sqlParams)
+//     .then(() => {
+//       console.log('task deleted');
+//       res.sendStatus(204);
+//     }).catch( (error) => {
+//       console.log(`Error making database query`, error);
+//       res.sendStatus(500); 
+//     })
+// })
 
 // Add record to db collection
-router.post('/:id', (req, res) => {
-  let recordId = req.params.id;
-  console.log('record added to collection', recordId);
-  let sqlQuery = `
-  INSERT INTO "records" 
-	  ("user_id", "record_id", "cover", "title", "year", "country", "genre", "style", "owned")
-  VALUES 
-		($1, $2, $3, $4, $5, $6, $7, $8, true), 
-  `;
-  const sqlParams = [
-  ];
-  pool.query(sqlQuery, sqlParams)
-    .then(() => {
-      console.log('task deleted');
-      res.sendStatus(204);
-    }).catch( (error) => {
-      console.log(`Error making database query`, error);
-      res.sendStatus(500); 
-    })
-})
+// router.post('/:id', (req, res) => {
+//   let recordId = req.params.id;
+//   console.log('record added to collection', recordId);
+//   let sqlQuery = `
+//   INSERT INTO "records" 
+// 	  ("user_id", "record_id", "cover", "title", "year", "country", "genre", "style", "owned")
+//   VALUES 
+// 		($1, $2, $3, $4, $5, $6, $7, $8, true), 
+//   `;
+//   const sqlParams = [
+//   ];
+//   pool.query(sqlQuery, sqlParams)
+//     .then(() => {
+//       console.log('task deleted');
+//       res.sendStatus(204);
+//     }).catch( (error) => {
+//       console.log(`Error making database query`, error);
+//       res.sendStatus(500); 
+//     })
+// })
 
 // Add record to db wishlist
-router.post('/:id', (req, res) => {
-  let recordId = req.params.id;
-  console.log('record added to wishlist', recordId);
-  let sqlQuery = `
-  INSERT INTO "records" 
-	  ("user_id", "record_id", "cover", "title", "year", "country", "genre", "style", "owned")
-  VALUES 
-		($1, $2, $3, $4, $5, $6, $7, $8, false), 
-  `;
-  const sqlParams = [
-  ];
-  pool.query(sqlQuery, sqlParams)
-    .then(() => {
-      console.log('task deleted');
-      res.sendStatus(204);
-    }).catch( (error) => {
-      console.log(`Error making database query`, error);
-      res.sendStatus(500); 
-    })
-})
+// router.post('/:id', (req, res) => {
+//   let recordId = req.params.id;
+//   console.log('record added to wishlist', recordId);
+//   let sqlQuery = `
+//   INSERT INTO "records" 
+// 	  ("user_id", "record_id", "cover", "title", "year", "country", "genre", "style", "owned")
+//   VALUES 
+// 		($1, $2, $3, $4, $5, $6, $7, $8, false), 
+//   `;
+//   const sqlParams = [
+//   ];
+//   pool.query(sqlQuery, sqlParams)
+//     .then(() => {
+//       console.log('task deleted');
+//       res.sendStatus(204);
+//     }).catch( (error) => {
+//       console.log(`Error making database query`, error);
+//       res.sendStatus(500); 
+//     })
+// })
 
 module.exports = router;
