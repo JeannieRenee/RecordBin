9e2ec4d71824384931732c8a69423f22e62533e2
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-07-05 12:47:19 -0500

    added api info to render

--- src/components/DetailsView/DetailsView.jsx
+++ src/components/DetailsView/DetailsView.jsx
@@ -10,19 +10,25 @@ function DetailsView(){
     return (
         <div className='detailed-view'>
             { record.title && <p>{record.title} - {record.artists_sort}</p> }
-            <p>{record.country} version released in {record.year}.</p>
+            { record.country && <p>{record.country} version released in {record.year}.</p>}
+            { record.community.want && <p> Want: {record.community.want} </p> }
+            { record.community.have && <p> Have: {record.community.have}</p> }
+            { record.community.rating && <p>Rating: {record.community.rating.average} out of {record.community.rating.count} votes</p> }
             { record.labels.name && <p>Label: {record.labels.name}</p> }
             { record.genres && <p>Genres: {record.genres}</p> }
             { record.styles && <p>Styles: {record.styles}</p> }
-            { record.tracklist && <><p>Tracks:</p>
-                <ul>
-                    {record.tracklist.map(track => {
-                        return (
-                        <li>{track.position} - {track.title}</li>
-                        )
-                    })}
-                </ul> 
-            </>}
+            { record.identifiers[0] && <p>Barcode: {record.identifiers[0].value}</p> }
+            { record.tracklist &&
+                <>
+                    <p>Tracks:</p>
+                    <ul>
+                        {record.tracklist.map(track => {
+                            return (
+                            <li>{track.position} - {track.title}</li>
+                            )
+                        })}
+                    </ul> 
+                </>}
             { record.notes && <p>Notes: {record.notes}</p> }
         </div>
     )
