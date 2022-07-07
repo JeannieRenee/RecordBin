4c32fe2fd2e770ffcda4a01246665533c2c22646
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-07-07 16:37:55 -0500

    idk

--- src/redux/reducers/records.reducer.js
+++ src/redux/reducers/records.reducer.js
@@ -3,8 +3,6 @@ const records = (state = [], action) => {
     switch (action.type) {
         case 'SET_RECORDS':
             return action.payload;
-        case 'RESET_RECORDS':
-            return []
         default:
             return state;
     }
